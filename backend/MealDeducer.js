f77f130a877d1d50dbc46472be6b7e03e91db852
KhDan007 <kh.dan007@gmail.com>
2024-05-25 11:19:00 +0500

    finish MealDeducer class

--- backend/MealDeducer.js
+++ backend/MealDeducer.js
@@ -20,19 +20,6 @@ class MealDeducer {
     }
   }
 
-  async getGroqChatCompletion() {
-    return this.groq.chat.completions.create({
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are the best cook in the world. You utilize advanced natural language processing techniques for semantic analysis and contextual understanding. You must be able to analyze food images and infer additional details about the recognized ingredients. For example, if the image contains 'lettuce' and 'tomato,' the system should recognize them as common ingredients in a salad and provide relevant information about salad recipes, dressings, etc. Furthermore, the system should consider the context in which the food items appear to enhance accuracy and provide more detailed information about the dish. For instance, if 'lettuce,' 'tomato,' and 'bread' are recognized together, the system should infer that it's likely a sandwich or burger and provide relevant details accordingly. Implement these features to create a comprehensive food analysis system that offers users rich and informative insights into the dishes depicted in the images.\n\nAll you response must be in this JSON format:\n{\n[name]: String\n[explanation]: String\n[ingredients (with units)]: Array\n[recipe (step by step detailed plan)]: String\n}",
-        },
-      ],
-      model: "llama3-8b-8192",
-    });
-  }
-
   async main() {
     try {
       await this.initConcepts(); // Wait for concepts to be initialized
@@ -40,13 +27,15 @@ class MealDeducer {
       // Check if this.concepts is defined and not empty
       if (this.concepts && Object.keys(this.concepts).length > 0) {
         // Construct a query based on the recognized ingredients (concepts)
-        const query = Object.entries(this.concepts)
+        this.query = await Object.entries(this.concepts)
           .map(([ingredient, confidence]) => `${ingredient}: '${confidence}'`)
           .join(",\n");
 
-        console.log(query);
+        console.log(this.query);
 
         this.chatCompletion = await this.getGroqChatCompletion();
+        this.output = this.chatCompletion.choices[0]?.message?.content || "";
+        console.log(this.output);
       } else {
         console.log("No recognized concepts found.");
       }
@@ -55,12 +44,23 @@ class MealDeducer {
       return null;
     }
   }
-}
 
-const systemPrompt = {
-  role: "system",
-  content:
-    "You are the best cook in the world. You utilize advanced natural language processing techniques for semantic analysis and contextual understanding. You must be able to analyze food images and infer additional details about the recognized ingredients. For example, if the image contains 'lettuce' and 'tomato,' the system should recognize them as common ingredients in a salad and provide relevant information about salad recipes, dressings, etc. Furthermore, the system should consider the context in which the food items appear to enhance accuracy and provide more detailed information about the dish. For instance, if 'lettuce,' 'tomato,' and 'bread' are recognized together, the system should infer that it's likely a sandwich or burger and provide relevant details accordingly. Implement these features to create a comprehensive food analysis system that offers users rich and informative insights into the dishes depicted in the images.\n\nAll you response must be in this JSON format:\n{\n[name]: String\n[explanation]: String\n[ingredients (with units)]: Array\n[recipe (step by step detailed plan)]: String\n}",
-};
+  async getGroqChatCompletion() {
+    return this.groq.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are the best cook in the world. You utilize advanced natural language processing techniques for semantic analysis and contextual understanding. You must be able to analyze food images and infer additional details about the recognized ingredients. For example, if the image contains 'lettuce' and 'tomato,' the system should recognize them as common ingredients in a salad and provide relevant information about salad recipes, dressings, etc. Furthermore, the system should consider the context in which the food items appear to enhance accuracy and provide more detailed information about the dish. For instance, if 'lettuce,' 'tomato,' and 'bread' are recognized together, the system should infer that it's likely a sandwich or burger and provide relevant details accordingly. Implement these features to create a comprehensive food analysis system that offers users rich and informative insights into the dishes depicted in the images.\n\nAll you response must be in this JSON format:\n{\n[name]: String\n[explanation]: String\n[ingredients (with units)]: Array\n[recipe (step by step detailed plan)]: String\n}",
+        },
+        {
+          role: "user",
+          content: this.query,
+        },
+      ],
+      model: "llama3-8b-8192",
+    });
+  }
+}
 
 module.exports = MealDeducer;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 const MealDeducer = require("./MealDeducer")
 
-const deducer = new MealDeducer("img/cheeseburger.jpg", 0.1);
+const deducer = new MealDeducer("img/chicken-broccoli.jpg", 0.1);
 deducer.main();
